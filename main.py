285679bacc8bbb295bf423ee3b122bbd87441a65
alejofabregas <afabregas@fi.uba.ar>
2025-10-30 00:58:22 -0300

    Add CORS middleware to FastAPI app
    Configured CORSMiddleware to allow requests from the local Next.js dev server and the production frontend. This enables cross-origin requests for specified methods and headers, improving frontend-backend integration.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Sets up FastAPI app, router, and exception handlers.
 import logging
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from visionf1.router.router import router as course_router
 
 environment = os.getenv('ENVIRONMENT', 'development')
@@ -25,6 +26,19 @@ logger = logging.getLogger(__name__)
 app = FastAPI(
     title = "VisionF1 Content Service API",
     version = "1.0.0",
+)
+
+allowed_origins = [
+    "http://localhost:3000",  # Next.js dev server
+    "https://visionf1.vercel.app" # Production frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
 )
 
 logger.info("Including routers...")
